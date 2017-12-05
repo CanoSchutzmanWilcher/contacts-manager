55ab440cab74bf912100f4ff5014257d56305c31
sschutzman <$t735815>
2017-12-05 15:01:21 -0600

    Need to sout out the menu

---
+++ src/Contacts.java
@@ -0,0 +1,33 @@
+public class Contacts {
+    private String lastname, firstname, phonenumber;
+
+    public Contacts(String lastname, String firstname, String phonenumber) {
+        this.lastname = lastname;
+        this.firstname = firstname;
+        this.phonenumber = phonenumber;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+}
